fff5e2c0576c6754d4c338099c6e632ef85adf64
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-04 21:37:19 +0200

    Update colors.py

--- colors.py
+++ colors.py
@@ -3,22 +3,24 @@ from colorama import Fore, Style, init
 # Colorama module's initialization.
 init(autoreset=True)
 
-def print_info(title, extra=''):
-    print(
+def print_info(title, extra='', start='', end='\n'):
+    print(start +
         Style.BRIGHT +
         Fore.WHITE + "[" +
         Fore.GREEN + "+" +
         Fore.WHITE + "] " +
         Fore.GREEN + title +
-        Fore.YELLOW + extra
-           )
+        Fore.YELLOW + extra,
+        end=end
+        )
 
-def print_error(title, extra=''):
-    print(
+def print_error(title, extra='', start='', end='\n'):
+    print(start +
         Style.BRIGHT +
         Fore.WHITE + "[" +
-        Fore.RED + "+" +
+        Fore.RED + "-" +
         Fore.WHITE + "] " +
         Fore.RED + title +
-        Fore.YELLOW + extra
-           )
+        Fore.YELLOW + extra,
+        end=end
+        )
