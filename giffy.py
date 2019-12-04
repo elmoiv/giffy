8a53bb3b01f1262422c760270c6870b0b0e6c2c8
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-04 17:33:20 +0200

    New Updates!
    - Improved printed info.
    - Handled `KeyboardInterrupt`
    - Fixed URLs without schemes.
    - Handled scraping errors.

--- giffy.py
+++ giffy.py
@@ -5,12 +5,17 @@ websites = data.data
 convert = converter.convert
 download = downloader.download
 
-def dl(url_input, optimize_gif=False):
+def main(url_input, optimize_gif=False):
     # Last "/" not needed
     if url_input[-1] == '/':
         url_input = url_input[:-1]
+    
+    # Apply scheme to avoid error
+    if 'http' not in url_input:
+        url_input = 'https://' + url_input
 
     GIF_RES = None
+    soup = None
 
     for one in websites:
         child = websites[one]
@@ -20,18 +25,27 @@ def dl(url_input, optimize_gif=False):
         # Check if url matches existed provider
         if re.match(r, url_input):
 
-            print_info('Detected: ', one)
+            print_info('Provider: ', one)
+            print_info('Optimzation: ', 'ON' * optimize_gif + 'OFF' * (not optimize_gif))
+
             # Getting ID to use as a name for the GIF
-            ID = re.split(r, url_input)[idx]
+            ID = re.split(r, url_input)[idx].replace('/', '-')
 
             # If scrape is True
             # We perform a given script via eval() to get media url
             if child['scrape']:
-                print_info('', 'Scraping...')
-                page = requests.get(url_input).text
-                soup = scraper.b(page)
-                GIF_RES = eval(child['scrape'])
+                print_info('Scraping: ', 'In Progress...')
+
+                if 'soup' in child['scrape']:
+                    page = requests.get(url_input).text
+                    soup = scraper.b(page)
+                try:
+                    GIF_RES = eval(child['scrape'])
+                except:
+                    print_error('Error: ', 'Scraper found nothing!')
+                    return
             else:
+                print_info('Scraping: ', 'Skipped')
                 # if not scrape, then we perform direct download
                 # using "op" key in data.py
                 GIF_RES = child['op'].format(ID)
@@ -52,15 +66,25 @@ def dl(url_input, optimize_gif=False):
         download(GIF_RES, name)
 
         if child['ext'] != 'gif':
-            print_info('', 'Converting MP4 to GIF...')
+            print_info('Converting: ', 'In Progress...')
             convert(name, optimize_gif)
+        else:
+            print_info('Converting: ', 'Skipped')
 
         # Clean downloaded videos after converting
         if name.split('.')[-1] != 'gif':
             os.remove(name)
 
-        print_info('', 'GIF Saved!')
+        print_info('Result: ', 'Success!')
         return
 
     print_error('Error: ','Provider is not supported :(')
     return
+
+def dl(a, b=False):
+    try:
+        main(a, b)
+    except KeyboardInterrupt:
+        print_error('Result: ', 'Interrupted!')
+
+dl(input('URL: '), True)
