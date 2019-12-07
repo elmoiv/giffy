fb9bb224e5f4faf64570fafa1365af070527e94f
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-07 11:17:47 +0200

    Clean style

--- colors.py
+++ colors.py
@@ -6,21 +6,19 @@ init(autoreset=True)
 def print_info(title, extra='', start='', end='\n'):
     print(start +
         Style.BRIGHT +
-        Fore.WHITE + "[" +
-        Fore.GREEN + "+" +
-        Fore.WHITE + "] " +
-        Fore.GREEN + title.ljust(15) +
-        Fore.YELLOW + extra,
-        end=end
-        )
+        Fore.WHITE   + "["  +
+        Fore.GREEN   + "+"  +
+        Fore.WHITE   + "] " +
+        Fore.GREEN   + title.ljust(15) +
+        Fore.YELLOW  + extra,
+        end=end)
 
 def print_error(title, extra='', start='', end='\n'):
     print(start +
         Style.BRIGHT +
-        Fore.WHITE + "[" +
-        Fore.RED + "-" +
-        Fore.WHITE + "] " +
-        Fore.RED + title.ljust(15) +
-        Fore.YELLOW + extra,
-        end=end
-        )
+        Fore.WHITE   + "["  +
+        Fore.RED     + "-"  +
+        Fore.WHITE   + "] " +
+        Fore.RED     + title.ljust(15) +
+        Fore.YELLOW  + extra,
+        end=end)
