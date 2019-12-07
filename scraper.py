eb9e6791be2330cf40c1d4f16060bcb0fc3af232
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-07 11:16:11 +0200

    Major Update!
    - Whole scraping process will be done in scraper.py
    - URL checking imported from giffy.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,32 @@
-import bs4
+import bs4, requests, re
+from colors import print_error
 
-def b(page):
-    return bs4.BeautifulSoup(page, "html.parser")
+def extract_url(url, script):
+    url_input = url
+    soup = None
+
+    if 'soup' in script:
+        try:
+            page = requests.get(url).text
+            soup = bs4.BeautifulSoup(page, "html.parser")
+        except requests.exceptions.ConnectionError:
+            print_error('Result: ', 'Connection Error!')
+            exit()
+    try:
+        return eval(script)
+    except:
+        print_error('Result: ', 'Scraper found nothing!')
+        exit()
+
+def check_url(url):
+    try:
+        
+        # Using requests.head to check if file exists without downloading
+        # https://stackoverflow.com/a/41546847/5305953
+        if requests.head(url).status_code != 200:
+            print_error('Result: ', 'No MP4 or GIF found!')
+            exit()
+    
+    except requests.exceptions.ConnectionError:
+        print_error('Result: ', 'Connection Error!')
+        exit()
