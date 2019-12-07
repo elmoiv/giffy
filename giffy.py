152fd73a33dbd92f5235ec936fb930e7188f87b5
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2019-12-07 11:14:41 +0200

    Medium Update!
    - URL checking exported to scraper.py
    - Removed slash checking (not needed)

--- giffy.py
+++ giffy.py
@@ -1,4 +1,5 @@
-import re, os, requests, data, scraper, converter, downloader
+import re, os, requests, data, converter, downloader
+from scraper import extract_url, check_url
 from colors import print_error, print_info
 
 websites = data.data
@@ -7,24 +8,19 @@ download = downloader.download
 
 def main(url_input, optimize_gif=False):
     print_info('Analyzing: ', url_input)
-    
-    # Last "/" not needed
-    if url_input[-1] == '/':
-        url_input = url_input[:-1]
-    
+
     # Apply scheme to avoid error
     if 'http' not in url_input:
         url_input = 'https://' + url_input
 
     GIF_RES = None
-    soup = None
 
     for one in websites:
         child = websites[one]
 
         r, idx = child['re']
 
-        # Check if url matches existed provider
+        # Check if url matches an existed provider
         if re.match(r, url_input):
 
             print_info('Provider: ', one)
@@ -37,15 +33,7 @@ def main(url_input, optimize_gif=False):
             # We perform a given script via eval() to get media url
             if child['scrape']:
                 print_info('Scraping: ', 'In Progress...')
-
-                if 'soup' in child['scrape']:
-                    page = requests.get(url_input).text
-                    soup = scraper.b(page)
-                try:
-                    GIF_RES = eval(child['scrape'])
-                except:
-                    print_error('Error: ', 'Scraper found nothing!')
-                    return
+                GIF_RES = extract_url(url_input, child['scrape'])
             else:
                 print_info('Scraping: ', 'Skipped')
                 # if not scrape, then we perform direct download
@@ -54,16 +42,14 @@ def main(url_input, optimize_gif=False):
 
         else:
             continue
-        
+
         # Full name with extension
         name = '{}.{}'.format(ID, child['ext'])
 
-        # Using requests.head to check if file exists without downloading
-        # https://stackoverflow.com/a/41546847/5305953
-        if requests.head(GIF_RES).status_code != 200:
-            print_error('Error: ', 'Connection Error!')
-            return
+        # Check if url is connectable
+        check_url(GIF_RES)
 
+        # Download mp4/GIF
         download(GIF_RES, name)
 
         if child['ext'] != 'gif':
@@ -79,7 +65,7 @@ def main(url_input, optimize_gif=False):
         print_info('Result: ', 'Success!')
         return
 
-    print_error('Error: ','Provider is not supported :(')
+    print_error('Result: ','Provider is not supported :(')
     return
 
 def dl(a, b=False):
