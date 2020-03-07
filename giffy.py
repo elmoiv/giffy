ee06529f9661ef414b4720f3ec1967c09d05a74f
Khaled ElMorshedy <31712173+elmoiv@users.noreply.github.com>
2020-03-07 20:58:51 +0200

    Added temporary testing

--- giffy.py
+++ giffy.py
@@ -1,79 +1,82 @@
 import re, os, requests, data, converter, downloader
 from scraper import extract_url, check_url
-from colors import print_error, print_info
+from colors import Printer
 
 websites = data.data
 convert = converter.convert
 download = downloader.download
 
-def main(url_input, optimize_gif=False):
-    print_info('Analyzing: ', url_input)
+# Run Printer
+log = Printer()
 
-    # Apply scheme to avoid error
-    if 'http' not in url_input:
-        url_input = 'https://' + url_input
+def main(url_input, optimize_gif=False, enable_logging=True):
+    # Enable or disable logging
+    log.LOG = enable_logging
 
-    GIF_RES = None
+    try:
+        log.print_info('Analyzing: ', url_input)
 
-    for one in websites:
-        child = websites[one]
+        # Apply scheme to avoid error
+        if 'http' not in url_input:
+            url_input = 'https://' + url_input
 
-        r, idx = child['re']
+        GIF_RES = None
 
-        # Check if url matches an existed provider
-        if re.match(r, url_input):
+        for one in websites:
+            child = websites[one]
 
-            print_info('Provider: ', one)
-            print_info('Optimzation: ', 'ON' * optimize_gif + 'OFF' * (not optimize_gif))
+            r, idx = child['re']
 
-            # Getting ID to use as a name for the GIF
-            ID = re.split(r, url_input)[idx].replace('/', '-')
+            # Check if url matches an existed provider
+            if re.match(r, url_input):
 
-            # If scrape is True
-            # We perform a given script via eval() to get media url
-            if child['scrape']:
-                print_info('Scraping: ', 'In Progress...')
-                GIF_RES = extract_url(url_input, child['scrape'])
+                log.print_info('Provider: ', one)
+                log.print_info('Optimzation: ', 'ON' * optimize_gif + 'OFF' * (not optimize_gif))
+
+                # Getting ID to use as a name for the GIF
+                ID = re.split(r, url_input)[idx].replace('/', '-')
+
+                # If scrape is True
+                # We perform a given script via eval() to get media url
+                if child['scrape']:
+                    log.print_info('Scraping: ', 'In Progress...')
+                    GIF_RES = extract_url(url_input, child['scrape'])
+                else:
+                    log.print_info('Scraping: ', 'Skipped')
+                    # if not scrape, then we perform direct download
+                    # using "op" key in data.py
+                    GIF_RES = child['op'].format(ID)
+
             else:
-                print_info('Scraping: ', 'Skipped')
-                # if not scrape, then we perform direct download
-                # using "op" key in data.py
-                GIF_RES = child['op'].format(ID)
+                continue
 
-        else:
-            continue
+            # Full name with extension
+            name = '{}.{}'.format(ID, child['ext'])
 
-        # Full name with extension
-        name = '{}.{}'.format(ID, child['ext'])
+            # Check if url is connectable
+            check_url(GIF_RES)
 
-        # Check if url is connectable
-        check_url(GIF_RES)
+            # Download mp4/GIF
+            download(GIF_RES, name)
 
-        # Download mp4/GIF
-        download(GIF_RES, name)
+            if child['ext'] != 'gif':
+                log.print_info('Converting: ', 'In Progress...')
+                convert(name, optimize_gif)
+            else:
+                log.print_info('Converting: ', 'Skipped')
 
-        if child['ext'] != 'gif':
-            print_info('Converting: ', 'In Progress...')
-            convert(name, optimize_gif)
-        else:
-            print_info('Converting: ', 'Skipped')
+            # Clean downloaded videos after converting
+            if name.split('.')[-1] != 'gif':
+                os.remove(name)
 
-        # Clean downloaded videos after converting
-        if name.split('.')[-1] != 'gif':
-            os.remove(name)
+            log.print_info('Result: ', 'Success!')
+            return
 
-        print_info('Result: ', 'Success!')
+        log.print_error('Result: ','Provider is not supported :(')
         return
 
-    print_error('Result: ','Provider is not supported :(')
-    return
-
-def dl(a, b=False):
-    try:
-        main(a, b)
     except KeyboardInterrupt:
-        print_error('Result: ', 'Interrupted!')
+        log.print_error('Result: ', 'Interrupted!')
 
-# if file is executed rather than imported
 if __name__ == '__main__':
-    dl(input('URL: '), True)
+    main(input('URL: '), True, True)
